8dfa1147a7490d29f16fc5312adf1e6a1493aa6b
ldd1278073960 <d1278073960>
2019-09-17 19:40:00 +0800

    主分支测试

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,15 @@
 
+/**
+ * @author Administrator
+ */
 public class Test {
 
+    public static void main(String[] args) {
 
+        System.out.println("123456");
+
+        System.out.println("cat");
+
+        System.out.println("dog");
+    }
 }
